788591467e84b0c2d950f5adc8c0f0101d1ce78f
Kukiko <33402648+Kukiko@users.noreply.github.com>
2018-08-28 20:49:29 +0200

    Add files via upload

---
+++ ali_macker.py
@@ -0,0 +1,43 @@
+
+
+print ("==============> VR1 <=============") 
+
+def mix():
+           print("search in google")
+           print("search about game")
+           print("play game") 
+
+
+def ali_macker(name ) :
+           
+           print ("Hi")
+           print ("ali")
+           print ("it;'s me")
+           print ("rem")
+           print ("yep")
+           print ("go fuck you")
+           print(" MR , please search in google " .format(name)) 
+
+def baka_fucker(name ) :
+           
+           print "he youtuper"
+           print "he not love anything <3"
+           print "he drinke the cold water"
+           print "he not love the coffe"
+           print(" MR , please search in google in amir_fucker".format(name)) 
+
+stop = False
+
+while (stop !=  True) :
+
+
+           wake_up  = raw_input ("write here your name :  ")
+
+           if (wake_up == "ahmed"):
+                                 baka_fucker()
+
+           elif wake_up == "amir" :
+                               print ( ali_mucker() ) 
+           else:
+                      print "nop"
+                      stop = True 
